74e1d0ce11391c78076671205913706fd93491ad
johandev21 <johan.p.2160@gmail.com>
2024-10-08 22:27:58 -0300

    config(db): cambiar la configuración de la base de datos a MySQL
    - Se ha actualizado el motor de la base de datos de SQLite a MySQL para mejorar la escalabilidad y el rendimiento.
    - Se ha configurado el nombre de la base de datos como 'foros'.
    - Se han añadido los parámetros de usuario y contraseña para la conexión a la base de datos.
    - Se ha importado la librería `pymysql` para facilitar la conexión a MySQL desde Django.

--- sistema_de_foros/settings.py
+++ sistema_de_foros/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'sistema_de_foros.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+import pymysql
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'foros',
+        'USER': 'root',
+        'PASSWORD': ''
     }
 }
 
