21244a510bb1ed4f076eef96bd030ca5cf7a188a
johandev21 <johan.p.2160@gmail.com>
2024-10-08 22:21:22 -0300

    feat(admin): registrar modelos en el panel de administración
    Se han agregado los modelos Usuario, Tematica, Foro, Post, Historial y Palabrotas al panel de administración de Django.

--- foros/admin.py
+++ foros/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Usuario, Tematica, Foro, Post, Historial, Palabrotas
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Tematica)
+admin.site.register(Foro)
+admin.site.register(Post)
+admin.site.register(Historial)
+admin.site.register(Palabrotas)
