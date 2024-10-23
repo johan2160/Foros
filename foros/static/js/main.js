3cd545cb180f78c872b7f7e3cc46c353aba8fcf5
johandev21 <johan.p.2160@gmail.com>
2024-10-23 19:51:19 -0300

    style: mejorar la legibilidad del código JavaScript
    - Se eliminaron espacios innecesarios para mejorar la consistencia y la legibilidad del código.
    - Se mantuvo la funcionalidad existente sin cambios.

--- foros/static/js/main.js
+++ foros/static/js/main.js
@@ -1,13 +1,14 @@
 const profileImage = document.querySelector('#profile-image')
-    const navigationMenu = document.querySelector('#navigation-menu')
+const navigationMenu = document.querySelector('#navigation-menu')
 
-    profileImage.addEventListener('click', (event) => {
-      navigationMenu.classList.toggle('hidden')
-      event.stopPropagation()
-    })
+profileImage.addEventListener('click', (event) => {
+  navigationMenu.classList.toggle('hidden')
+  event.stopPropagation()
+})
 
-    window.addEventListener('click', (event) => {
-      if (!navigationMenu.contains(event.target)) {
-        navigationMenu.classList.add('hidden')
-      }
-    })
+window.addEventListener('click', (event) => {
+  if (!navigationMenu.contains(event.target)) {
+    navigationMenu.classList.add('hidden')
+  }
+})
+
