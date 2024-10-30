bf5351456077837a67f1813248ff305e61255b1f
johandev21 <johan.p.2160@gmail.com>
2024-10-30 12:54:40 -0300

    Organización y actualización de rutas en urls.py para mejorar la estructura y funcionalidad.
    - Añadidos comentarios para agrupar las rutas según su funcionalidad:
      - Login/Signup/Logout
      - Manejo de Usuarios
      - Manejo de Temáticas
      - Manejo de Foros
      - Manejo de Publicaciones
      - Manejo de Historial de Acciones
    - Agregadas rutas para mostrar y eliminar publicaciones:
      - `foro/<int:foro_id>/publicacion/<int:publicacion_id>` asignado a la vista `mostrarPublicacion`, nombre `mostrar_publicacion`.
      - `foro/<int:foro_id>/eliminar_publicacion/<int:publicacion_id>` asignado a la vista `eliminarPublicacion`, nombre `eliminar_publicacion`.
    - Reorganizadas las rutas existentes para mejorar la legibilidad y mantenimiento del código.
    
    Estos cambios facilitan la navegación y gestión de publicaciones, y mejoran la comprensión del archivo de rutas.

--- sistema_de_foros/urls.py
+++ sistema_de_foros/urls.py
@@ -6,7 +6,8 @@ from foros import views
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
-    
+
+    # ---------- Login/Signup/Logout ----------    
     path('', views.mostrarIndex, name='index'),
     path('login/', views.mostrarLogin, name='login'),
     path('form_login/', views.formLogin, name='form_login'),
@@ -14,19 +15,21 @@ urlpatterns = [
     path('signup/', views.mostrarSignup, name='signup'),
     path('form_signup/', views.formSignup, name='form_signup'),
     
+    # ---------- Manejo de Usuarios ----------    
     path('perfil_usuario/<int:id>/', views.mostrarPerfilUsuario, name='perfil_usuario'),
     path('editar_usuario/<int:id>', views.mostrarEditarPerfilUsuario, name='editar_usuario'),
     path('form_editar_usuario/<int:id>', views.formEditarPerfilUsuario, name='form_editar_usuario'),
     path('gestionar_usuarios/', views.mostrarGestionarUsuarios, name='gestionar_usuarios'),
     
+    # ---------- Manejo de Tematicas ----------    
     path('administrar_tematicas/', views.mostrarAdministrarTematicas, name='administrar_tematicas'),
     path('crear_tematica/', views.mostrarCrearTematica, name='crear_tematica'),
     path('form_crear_tematica/', views.formCrearTematica, name='form_crear_tematica'),
     path('editar_tematica/<int:id>', views.mostrarEditarTematica, name='editar_tematica'),
     path('form_editar_tematica/<int:id>', views.formEditarTematica, name='form_editar_tematica'),
     path('eliminar_tematica/<int:id>', views.eliminarTematica, name='eliminar_tematica'),
-
     
+    # ---------- Manejo de Foros ----------    
     path('foro/<int:foro_id>', views.mostrarForo, name='foro'),
     path('crear_foro/', views.mostrarCrearForo, name='crear_foro'),
     path('form_crear_foro/', views.formCrearForo, name='form_crear_foro'),
@@ -35,11 +38,15 @@ urlpatterns = [
     path('administrar_foros/', views.mostrarAdministrarForos, name='administrar_foros'),
     path('eliminar_foro/<int:id>', views.eliminarForo, name='eliminar_foro'),
     
+    # ---------- Manejo de Publicaciones ----------    
     path('foro/<int:foro_id>/crear_publicacion', views.mostrarCrearPublicacion, name='crear_publicacion'),
     path('foro/<int:foro_id>/form_crear_publicacion', views.formCrearPublicacion, name='form_crear_publicacion'),
     path('foro/<int:foro_id>/editar_publicacion/<int:publicacion_id>', views.mostrarEditarPublicacion, name='editar_publicacion'),
     path('foro/<int:foro_id>/form_editar_publicacion/<int:publicacion_id>', views.formEditarPublicacion, name='form_editar_publicacion'),
+    path('foro/<int:foro_id>/publicacion/<int:publicacion_id>', views.mostrarPublicacion, name='mostrar_publicacion'),
+    path('foro/<int:foro_id>/eliminar_publicacion/<int:publicacion_id>', views.eliminarPublicacion, name='eliminar_publicacion'),
     
+    # ---------- Manejo de Historial de Acciones ----------    
     path('historial_acciones/', views.mostrarHistorialAcciones, name='historial_acciones'),
 ]
 
